f851f904c0e248df9e5430d8aa2baad164f09a11
joão vinícius <j.vin7cius@gmail.com>
2023-01-21 20:21:17 -0300

    introdução

--- fibo.py
+++ fibo.py
@@ -3,6 +3,8 @@ a, b, i = 0, 1, 0
 count = int(input('Quantos numeros da sequencia deseja exibir? '))
 
 while i < count:
-    print(a)
+    print(a, end=' - ')
     a, b = b, a + b
     i += 1 
+
+print('...')
---
+++ introducion.py
@@ -0,0 +1,56 @@
+product = 20
+tax = 0.10
+price = product * (1 + tax)
+
+print(price)
+
+# números complexos
+
+calc_complex = (5+3j) + (4+2j)
+print(calc_complex)
+
+print(complex(10,2))
+
+# strings
+
+string = 'doesn\'t'
+print(string)
+print(r'\a\n\r')
+
+literal = '''\
+                        welcome, _______
+    user ----------------------------------- vinicius-arg
+    password ------------------------------- 12345678
+    email ---------------------------------- tantantan@gmail.com
+'''
+print(literal)
+
+chika = 5 * 'shuki ' + 'setee'
+print(chika)
+
+name = 'Back yi-jin'
+print(name[:5] + name[5:])
+
+other_name = 'N' + name[1:]
+print(other_name)
+
+print(len(name))
+
+#listas
+
+squares = [1, 4, 9, 16, 25]
+
+# new_squares = squares
+# new_squares.append(36)
+# print(new_squares)
+# print(squares)
+# o valor foi passado como referência, em vez disso:
+
+new_squares = squares[:]
+new_squares.append(36)
+print(new_squares)
+
+squares[:2] = ['um', 'quatro']
+print(squares)
+
+print(len(squares))
