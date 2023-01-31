6328ba258c6052bce170948beb7f95a031ac1aae
joão vinícius <j.vin7cius@gmail.com>
2023-01-31 20:40:05 -0300

    import

---
+++ course/import.py
@@ -0,0 +1,14 @@
+import math
+import pandas as pd
+
+PI = math.pi
+
+dataFrame = pd.DataFrame({
+    'Ano': [2000, 2010, 2020],
+    'População': [7892, 8567, 10203]
+    })
+
+squareRoot = math.sqrt(9)
+
+print(dataFrame)
+print(squareRoot)
