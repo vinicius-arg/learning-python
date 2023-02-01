374e1ea3d032a41dc5b606d768f1c9cf99adc7d1
joão vinícius <j.vin7cius@gmail.com>
2023-01-31 21:43:11 -0300

    operators

---
+++ course/operators.py
@@ -0,0 +1,27 @@
+# mathematics
+# + - (-) * / % ** int() float()
+
+A = float(2**16)
+
+print(A)
+
+# relational
+# > < >= <= == !=
+
+B = A >= 2**15
+
+print(B)
+
+#assignment
+# = += -= *= /= **=
+
+A /= 2**15
+
+print(A)
+
+#logical
+#not and or
+
+C = not (A <= 3.0 and B)
+
+print(C)
