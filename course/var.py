7c8b2f6fa503c983ed79585d9e59d4d3172ea35c
joão vinícius <j.vin7cius@gmail.com>
2023-02-02 11:09:12 -0300

    variáveis

---
+++ course/var.py
@@ -0,0 +1,12 @@
+'''docstring: testes de aula sobre variáveis '''
+
+X = int(7.8)
+STRING = 'abcdef'  + 'ghijkl'
+print(f'Alfabeto: {STRING}')
+
+d1, d2, d3 = 3, 5, 0
+
+SLICING = STRING[6:]
+
+print(SLICING)
+print(STRING[::2])
