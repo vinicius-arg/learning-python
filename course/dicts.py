6a77523a4509690b3dd33c0906697ab41e186b3b
joão vinícius <j.vin7cius@gmail.com>
2023-02-05 15:02:44 -0300

    dict

--- course/dicts.py
+++ course/dicts.py
@@ -1,1 +1,20 @@
 '''Dicionários'''
+
+dicionario = {
+    'nome': None,
+    'idade': None
+    }
+
+print(dicionario.keys())
+
+nome = input('Digite um nome: ')
+idade = int(input('Digite uma idade: '))
+
+dic_transicao = {
+    'nome': nome,
+    'idade': idade
+}
+
+dicionario.update(dic_transicao)
+
+print(f'{dicionario["nome"]} tem {dicionario["idade"]} anos.')
